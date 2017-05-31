4b00d9a57c05f6a6a623a305c0ba4f0069d00b3b
Timofey Tyrin <timat-ti@mail.ru>
2017-05-31 10:36:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ int main()
     std::ifstream file;
     file.open("SomeFile");
 
-    std::map <std::string,int> words;
+    std::map<std::string,int> words;
     std::string word;
     while (file >> word)
     {
         words[word]++;
     }
 
-    std::map <std::string,int>::iterator it;
-    std::cout<<"Words count:"<<std::endl;
-    for (it = words.begin();it!=words.end();it++)
+    std::map<std::string,int>::iterator it;
+    std::cout << "Words count:" << std::endl;
+    for (it = words.begin(); it != words.end(); it++)
     {
         std::cout << (*it).first << ": " << (*it).second << std::endl;
     }
